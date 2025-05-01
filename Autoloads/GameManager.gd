c01ded8896ba045f7c0ae3414d98a375eae3b009
saltchicken <john.eicher89@gmail.com>
2025-04-30 19:55:30 -0700

    Combine GameManager and MultiplayerManager

--- Autoloads/GameManager.gd
+++ Autoloads/GameManager.gd
@@ -2,16 +2,19 @@ extends Node
 
 const SERVER_PORT = 30980
 # const SERVER_IP = "ec2-18-144-165-78.us-west-1.compute.amazonaws.com"
-const SERVER_IP = "127.0.0.1"
+const SERVER_IP = "main"
 
 var player_scene = preload("res://player.tscn")
-
 var _players_spawn_node
 
+func _ready():
+	if OS.has_feature("dedicated_server"):
+		print("Starting dedicated server...")
+		self.become_host()
+
 func become_host():
-	print("Starting host")
+	print("Game Manager host started")
 	
-	_players_spawn_node = get_tree().get_current_scene().get_node("Players")
 	
 	var server_peer = ENetMultiplayerPeer.new()
 	server_peer.create_server(SERVER_PORT)
@@ -20,26 +23,28 @@ func become_host():
 	
 	multiplayer.peer_connected.connect(_add_player_to_game)
 	multiplayer.peer_disconnected.connect(_del_player)
-	
+
+
 func player_join():
 	print("Joining game")
-	
+
 	var client_peer = ENetMultiplayerPeer.new()
 	client_peer.create_client(SERVER_IP, SERVER_PORT)
-	
+
 	multiplayer.multiplayer_peer = client_peer
-	
+
 func _add_player_to_game(id: int):
-	print("Player %s joined the game" % id)
+	_players_spawn_node = get_tree().get_current_scene().get_node("Players")
+	print("Player %s joining" % id)
 	var player_to_add = player_scene.instantiate()
 	player_to_add.player_id = id
 	player_to_add.name = str(id)
 	player_to_add.position = Vector3(0, 3, 0)
 	_players_spawn_node.add_child(player_to_add, true)
-	
+	print("Player %s joined" % id)
+
 func _del_player(id: int):
 	print("Player %s left the game" % id)
 	if not _players_spawn_node.has_node(str(id)):
 		return
 	_players_spawn_node.get_node(str(id)).queue_free()
-	
---
+++ pause_menu.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Join.pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+	self.hide()
+	GameManager.player_join()
+
