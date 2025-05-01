c613241dc5e88dac5f9dcaee19e63c9f59c918da
saltchicken <john.eicher89@gmail.com>
2025-05-01 12:52:35 -0700

    Get player_name from main_menu

--- main_menu.gd
+++ main_menu.gd
@@ -5,6 +5,9 @@ func _ready() -> void:
 	$Join.pressed.connect(_on_button_pressed)
 
 func _on_button_pressed():
+	var player_name = %Name.text
+	if player_name.strip_edges().is_empty():
+		player_name = "Player" + str(randi() % 1000)
 	self.hide()
 	GameManager.StartGame()
 	GameManager.player_join()
