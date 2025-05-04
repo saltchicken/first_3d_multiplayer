bd04ac3c74880d01fdcf64220887193034630799
saltchicken <john.eicher89@gmail.com>
2025-05-03 18:21:17 -0700

    Add clear

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,1 +1,1 @@
-Godot.x86_64 --headless --export-release "Dedicated Server" && ./dist/dedicated_server/dedicated_server.x86_64
+Godot.x86_64 --headless --export-release "Dedicated Server" && clear && ./dist/dedicated_server/dedicated_server.x86_64
