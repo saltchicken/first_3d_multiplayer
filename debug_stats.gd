16c311da60f11770327080c3353748827e877fe7
saltchicken <john.eicher89@gmail.com>
2025-05-04 17:55:47 -0700

    Remove excluded in debug_stats

--- debug_stats.gd
+++ debug_stats.gd
@@ -2,8 +2,21 @@ extends Node3D
 
 @export var spacing: float = 0.25
 @export var vertical: bool = true
+@export var excluded_names: Array[String] = []
 
 func _ready():
+	reposition_children()
+
+func is_authority_client():
+	var parent = get_parent()
+	return parent.role == parent.Role.AUTHORITY_CLIENT
+
+func remove_excluded_children():
+	for i in range(get_child_count() - 1, -1, -1):
+		var child = get_child(i)
+		print(child.name)
+		if excluded_names.has(child.name):
+			child.queue_free()
 	reposition_children()
 
 func reposition_children():
